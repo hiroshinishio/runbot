6856c1da6b89d109d3a0a1e38241be08d0f7fd2d
Christophe Monniez <moc@odoo.com>
2023-07-13 10:12:23 +0200

    starts a docker-registry at each builder starts

--- runbot/models/runbot.py
+++ runbot/models/runbot.py
@@ -1,3 +1,4 @@
+import docker
 import time
 import logging
 import glob
@@ -383,6 +384,38 @@ class Runbot(models.AbstractModel):
         if ignored:
             _logger.info('docker (%s) not deleted because not dest format', list(ignored))
 
+    def _start_docker_registry(self, host):
+        """
+        Start a docker registry if not already running.
+        The registry is in `always_restart` mode, meaning that it will restart properly after a reboot.
+        """
+        docker_client = docker.from_env()
+        try:
+            registry_container = docker_client.containers.get('runbot-registry')
+        except docker.errors.NotFound:
+            registry_container = None
+
+        if registry_container:
+            if registry_container.status in ('running', 'created', 'restarting'):
+                _logger.info('Docker registry container already found with status %s, skipping start procedure.', container.status)
+                return
+            else:
+                _logger.info('Docker registry container found with status %s, trying the start procedure.', container.status)
+
+        try:
+            container = docker_client.containers.run(
+                'registry:2',
+                name='runbot-registry',
+                volumes={f'{os.path.join(self._root(), "docker-registry")}':{'bind': '/var/lib/registry', 'mode': 'rw'}},
+                ports={5000: ('127.0.0.1', 5001)},
+                restart_policy= {"Name": "always"},
+                detach=True
+            )
+            _logger.info('Docker registry started')
+        except Exception as e:
+            message = f'Starting registry failed with exception: {e}'
+            self.warning(message)
+            _logger.error(message)
 
     def _warning(self, message, *args):
         if args:
